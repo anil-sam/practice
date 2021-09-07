94895b24e68346603731bcf12e6b0bfc53cc503f
Anil Shivakumar <anilani315@gmail.com>
2021-09-07 16:04:18 +0530

    Enter key added

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -5,6 +5,11 @@ function WeatherApp() {
 	const [city, setCity] = useState('Bangalore')
 	const [response, setResponse] = useState({})
 	
+	const onKey = (e) => {
+        if(e.key === "Enter") {
+            handleClick()
+        }
+    }
 	const handleClick = async () => {
 		if(city === '') return
 		const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=65f65ad51bc6a263c95e2a78516c9843`)
@@ -21,7 +26,7 @@ function WeatherApp() {
 
     return (
         <div>
-          	<input type="text" value={city} onChange={e => setCity(e.target.value)} />
+          	<input type="text" value={city} onChange={e => setCity(e.target.value)} onKeyPress={onKey}/>
 					<button onClick={handleClick}>Search</button>
 					{response?.cod == "200" && <>
 					<h1>Name: {response.name}</h1>
